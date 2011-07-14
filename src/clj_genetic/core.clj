854753ff8859c9c96225ed8055298f6326a2a8bc
Anton Beloglazov <anton.beloglazov@gmail.com>
2011-07-14 15:43:28 +1000

    A generic implementation of run

--- src/clj_genetic/core.clj
+++ src/clj_genetic/core.clj
@@ -1,1 +1,19 @@
-(ns clj-genetic.core)
+(ns clj-genetic.core
+  (:use clj-genetic.util))
+
+(defn run [evaluate selection recombine terminate? initial-population]
+  {:pre [(c (fn? evaluate))
+         (c (fn? selection))
+         (c (fn? recombine))
+         (c (fn? terminate?))
+         (c (coll? initial-population))]
+   :post [(c (map? %))]}
+  (loop [step 0
+         population initial-population]
+    (let [results (evaluate population)
+          selected-chromosomes (selection population)] 
+      (if (terminate? results step)
+        {:results results
+         :step step}
+        (recur (inc step)
+               (recombine selected-chromosomes))))))
--- test/clj_genetic/test/core.clj
+++ test/clj_genetic/test/core.clj
@@ -1,3 +1,25 @@
 (ns clj-genetic.test.core
   (:use clj-genetic.core
+        clj-genetic.util
         midje.sweet))
+
+(unfinished evaluate selection recombine terminate?)
+
+(def population [1 2 3])
+
+(fact
+  (run evaluate selection recombine terminate? population) => 
+  (just {:results anything
+         :step 0})
+  (provided 
+    (selection anything) => anything
+    (terminate? (evaluate population) 0) => true)
+  
+  (run evaluate selection recombine terminate? population) => 
+  (just {:results anything
+         :step 1})
+  (provided
+    (selection anything) => anything
+    (recombine anything) => anything
+    (terminate? (evaluate anything) 0) => false
+    (terminate? (evaluate anything) 1) => true))
