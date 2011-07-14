b6ea4c7b805ecf20035578e9469c11a26b66d084
Anton Beloglazov <anton.beloglazov@gmail.com>
2011-07-14 17:44:49 +1000

    Tournament selection completed

---
+++ src/clj_genetic/selection.clj
@@ -0,0 +1,35 @@
+(ns clj-genetic.selection
+  (:use clj-genetic.util))
+
+(defn tournament-select
+  "1. Any feasible solution is preferred to any infeasible solution.
+   2. Among two feasible solutions, the one having better objective function value is preferred.
+   3. Among two infeasible solutions, the one having smaller constraint violation is preferred."
+  [[% a-meta :as a] [% b-meta :as b]]
+  {:pre [(c (coll? a))
+         (c (contains? a-meta :fitness))
+         (c (coll? b))
+         (c (contains? b-meta :fitness))]
+   :post [(c (vector? %))]}
+  (cond 
+    (and (:feasible a-meta)
+         (:not-feasible b-meta)) a
+    (and (:not-feasible a-meta)
+         (:feasible b-meta)) b
+    (> (:fitness a-meta)
+       (:fitness b-meta)) a
+    :else b))
+
+(defn tournament
+  "Applies the tournament selection without replacement to select n chromosomes"
+  [chromosomes n]
+  {:pre [(c (map? chromosomes))]
+   :post [(c (map? %))]}
+  (let [chromosomes-vec (vec chromosomes)] 
+    (into {} 
+          (loop [selected-chromosomes #{}]
+            (if (= n (count selected-chromosomes))
+              selected-chromosomes
+              (recur (conj selected-chromosomes 
+                           (tournament-select (rand-nth chromosomes-vec)
+                                              (rand-nth chromosomes-vec)))))))))
---
+++ test/clj_genetic/test/selection.clj
@@ -0,0 +1,38 @@
+(ns clj-genetic.test.selection
+  (:use clj-genetic.selection
+        clj-genetic.util
+        midje.sweet))
+
+(fact
+  "1. Any feasible solution is preferred to any infeasible solution.
+   2. Among two feasible solutions, the one having better objective function value is preferred.
+   3. Among two infeasible solutions, the one having smaller constraint violation is preferred."
+  (first (tournament-select [.a. {:feasible true
+                                  :fitness 1}] 
+                            [.b. {:not-feasible true
+                                  :fitness 1}])) => .a.
+  (first (tournament-select [.a. {:not-feasible true
+                                  :fitness 1}] 
+                            [.b. {:feasible true
+                                  :fitness 1}])) => .b.
+  (first (tournament-select [.a. {:not-feasible true
+                                  :fitness 2}] 
+                            [.b. {:not-feasible true
+                                  :fitness 1}])) => .a.
+  (first (tournament-select [.a. {:not-feasible true
+                                  :fitness 1}] 
+                            [.b. {:not-feasible true
+                                  :fitness 2}])) => .b.
+  (first (tournament-select [.a. {:feasible true
+                                  :fitness 2}] 
+                            [.b. {:feasible true
+                                  :fitness 1}])) => .a.
+  (first (tournament-select [.a. {:feasible true
+                                  :fitness 1}] 
+                            [.b. {:feasible true
+                                  :fitness 2}])) => .b.)
+
+(fact
+  (count (tournament {.a. {:fitness 1}
+                      .b. {:fitness 2}
+                      .c. {:fitness 3}} 2)) => 2)
