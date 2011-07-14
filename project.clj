79794379aaf04aeec93b07fe9f8a2057c9accf17
Anton Beloglazov <anton.beloglazov@gmail.com>
2011-07-14 14:32:24 +1000

    Deps and git ignore

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject clj-genetic "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [midje "1.2-alpha4"]
+])
