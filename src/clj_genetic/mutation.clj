de0baf7a1c9065bd63442318213966b65e988ee3
Anton Beloglazov <anton.beloglazov@gmail.com>
2011-07-15 12:58:24 +1000

    parameter-based

--- src/clj_genetic/mutation.clj
+++ src/clj_genetic/mutation.clj
@@ -9,11 +9,25 @@
   (let [p (+ (/ 1 n) (* (/ t t-max) (- 1 (/ 1 n))))]
     (< (rand) p)))
 
-(defn parameter-based [genes t t-max]
+(defn parameter-based-mutate [gene limits t nu]
+  {:pre [(c (number? gene))
+         (c (contains-keys? limits :min :max))
+         (c (not-negnum? nu))
+         (c (not-negnum? t))]
+   :post [(c (number? %))]}
+  ())
+
+(defn parameter-based [genes limits t t-max]
   {:pre [(c (coll? genes))
+         (c (coll? limits))
          (c (not-negnum? t))
          (c (not-negnum? t-max))]
    :post [(c (coll? %))]}
-  (let [n (count genes)]
-    ))
+  (let [nu 100 ; from the paper
+        n (count genes)]
+    (map (fn [gene gene-limits]
+           (if (parameter-based-mutate? n t t-max)
+             (parameter-based-mutate gene gene-limits t nu)
+             gene))
+         genes limits)))
 
--- test/clj_genetic/test/mutation.clj
+++ test/clj_genetic/test/mutation.clj
@@ -3,20 +3,35 @@
         clj-genetic.util
         midje.sweet))
 
+(def genes [1 2 3])
+(def limits [{:min 1 :max 2}
+             {:min 2 :max 3}
+             {:min 3 :max 4}])
+(def t 5)
+(def t-max 500)
+(def n 3)
+
 (fact 
   "1÷3+(5÷500)(1 − 1÷3) = 0.34"
-  (let [n 3
-        t 5
-        t-max 500] 
-    (parameter-based-mutate? n t t-max) => true
-    (provided (rand) => 0.0)
-    (parameter-based-mutate? n t t-max) => true
-    (provided (rand) => 0.2)
-    (parameter-based-mutate? n t t-max) => true
-    (provided (rand) => 0.3)
-    (parameter-based-mutate? n t t-max) => false
-    (provided (rand) => 0.34)
-    (parameter-based-mutate? n t t-max) => false
-    (provided (rand) => 0.5)
-    (parameter-based-mutate? n t t-max) => false
-    (provided (rand) => 1.0)))
+  (parameter-based-mutate? n t t-max) => true
+  (provided (rand) => 0.0)
+  (parameter-based-mutate? n t t-max) => true
+  (provided (rand) => 0.2)
+  (parameter-based-mutate? n t t-max) => true
+  (provided (rand) => 0.3)
+  (parameter-based-mutate? n t t-max) => false
+  (provided (rand) => 0.34)
+  (parameter-based-mutate? n t t-max) => false
+  (provided (rand) => 0.5)
+  (parameter-based-mutate? n t t-max) => false
+  (provided (rand) => 1.0))
+
+(fact
+  (parameter-based genes limits t t-max) => genes
+  (provided (parameter-based-mutate? n t t-max) => false)
+  (parameter-based genes limits t t-max) => (just 5 5 5)
+  (provided (parameter-based-mutate? n t t-max) => true
+            (parameter-based-mutate anything
+                                    anything
+                                    t
+                                    anything) => 5))
