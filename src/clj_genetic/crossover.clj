38c11d3b3866baafb1a25c9267a494893db705fb
Anton Beloglazov <anton.beloglazov@gmail.com>
2011-07-15 16:15:45 +1000

    simulated-binary crossover with limits

--- src/clj_genetic/crossover.clj
+++ src/clj_genetic/crossover.clj
@@ -2,3 +2,40 @@
   (:use clj-genetic.util
         incanter.core))
 
+(defn simulated-binary-cross [gene1 gene2 limits nu]
+  {:pre [(c (number? gene1))
+         (c (number? gene2))
+         (c (contains-keys? limits :min :max))
+         (c (posnum? nu))]
+   :post [(c (coll? %))]}
+  (let [[x1 x2] (if (< gene1 gene2)
+                  [gene1 gene2]
+                  [gene2 gene1])
+        u (rand)
+        b ($= 1 + 2 / (x2 - x1) * (min (- x1 (:min limits)) (- (:max limits) x2)))
+        a ($= 2 - b ** (-1 * (nu + 1)))
+        beta (if (<= u (/ 1 a))
+               ($= (a * u) ** (1 / (nu + 1)))
+               ($= (1 / (2 - a * u)) ** (1 / (nu + 1))))
+        y1 ($= 0.5 * (x1 + x2 - beta * (Math/abs (- x2 x1))))
+        y2 ($= 0.5 * (x1 + x2 + beta * (Math/abs (- x2 x1))))]
+    (if (< gene1 gene2)
+      [y1 y2]
+      [y2 y1])))
+
+(defn simulated-binary
+  
+  ([chromosome1 chromosome2 limits]
+    {:pre [(c (coll? chromosome1))
+           (c (coll? chromosome2))
+           (c (coll? limits))]
+     :post [(c (coll? %))]}
+    (let [p 0.5 ; probability of selecting a gene for crossover
+          nu 1
+          new-genes (map (fn [gene1 gene2 gene-limits]
+                           (if (< (rand) p)
+                             (simulated-binary-cross gene1 gene2 gene-limits)
+                             [gene1 gene2]))
+                         chromosome1 chromosome2 limits)]
+      [(map first new-genes) (map second new-genes)])))
+
--- test/clj_genetic/test/crossover.clj
+++ test/clj_genetic/test/crossover.clj
@@ -3,3 +3,41 @@
         clj-genetic.util
         midje.sweet))
 
+(def genes [1 2 3])
+(def limits [{:min 1 :max 2}
+             {:min 2 :max 3}
+             {:min 3 :max 4}])
+(def nu 1)
+
+(fact 
+  "Simulated binary crossover with parameter limits
+  b = 1+(2/(x2-x1))*min((x1-min, max-x2))
+  = 1+(2/(5-1))*min((1-0, 30-5)) = 1+(2/(5-1))*1 = 1.5
+  a = 2-b^(-(nu+1)) = 2-1.5^(-(1+1)) = 1.555555556
+  1/a = 0.642857143
+  beta = (a*u)^(1/(nu+1)) = (1.555555556*0.3)^(1/(1+1)) = 0.683130051
+  y1 = 0.5*(x1+x2-beta*(x2-x1)) = 0.5*(1+5-0.683130051*(5-1)) = 1.633739898
+  y2 = 0.5*(x1+x2+beta*(x2-x1)) = 0.5*(1+5+0.683130051*(5-1)) = 4.366260102
+
+  beta = (1/(2-a*u)^(1/(nu+1))) = (1/(2-1.555555556*0.8)^(1/(1+1))) = 1.150447484
+  y1 = 0.5*(x1+x2-beta*(x2-x1)) = 0.5*(1+5-1.150447484*(5-1)) = 0.699105032
+  y2 = 0.5*(x1+x2+beta*(x2-x1)) = 0.5*(1+5+1.150447484*(5-1)) = 5.300894968"
+  (simulated-binary-cross 1 5 {:min 0 :max 30} nu) => (just
+                                                        (roughly 1.6337)
+                                                        (roughly 4.3662))
+  (provided (rand) => 0.3)
+  (simulated-binary-cross 1 5 {:min 0 :max 30} nu) => (just
+                                                        (roughly 0.6991)
+                                                        (roughly 5.3008))
+  (provided (rand) => 0.8)
+  (simulated-binary-cross 5 1 {:min 0 :max 30} nu) => (just                                                        
+                                                        (roughly 4.3662)
+                                                        (roughly 1.6337))
+  (provided (rand) => 0.3)
+  (simulated-binary-cross 5 1 {:min 0 :max 30} nu) => (just
+                                                        (roughly 5.3008)
+                                                        (roughly 0.6991))
+  (provided (rand) => 0.8))
+
+
+
