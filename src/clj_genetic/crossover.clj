ee3a166385a427a523e2c0a29caf77a6b4fcf43c
Anton Beloglazov <anton.beloglazov@gmail.com>
2011-07-20 14:42:05 +1000

    Added crossover probability parameter

--- src/clj_genetic/crossover.clj
+++ src/clj_genetic/crossover.clj
@@ -30,21 +30,24 @@
            (c (coll? chromosome1))
            (c (coll? chromosome2))]
      :post [(c (coll? %))]}
-    (simulated-binary-with-limits limits 0.5 1 chromosome1 chromosome2))
+    (simulated-binary-with-limits limits 0.9 0.5 1 chromosome1 chromosome2))
   
-  ([limits p nu chromosome1 chromosome2]
+  ([limits probability gene-probability nu chromosome1 chromosome2]
     {:pre [(c (coll? limits))
-           (not-negnum? p)
+           (not-negnum? probability)
+           (not-negnum? gene-probability)
            (not-negnum? nu)
            (c (coll? chromosome1))
            (c (coll? chromosome2))]
      :post [(c (coll? %))]}
-    (let [new-genes (map (fn [gene1 gene2 gene-limits]
-                           (if (< (rand) p)
-                             (simulated-binary-with-limits-cross gene-limits nu gene1 gene2)
-                             [gene1 gene2]))
-                         chromosome1 chromosome2 limits)]
-      [(map first new-genes) (map second new-genes)])))
+    (if (< (rand) probability) 
+      (let [new-genes (map (fn [gene1 gene2 gene-limits]
+                             (if (< (rand) gene-probability)
+                               (simulated-binary-with-limits-cross gene-limits nu gene1 gene2)
+                               [gene1 gene2]))
+                           chromosome1 chromosome2 limits)]
+        [(map first new-genes) (map second new-genes)])
+      [chromosome1 chromosome2])))
 
 (defn simulated-binary-cross [nu gene1 gene2]
   {:pre [(c (posnum? nu))
@@ -65,18 +68,25 @@
     {:pre [(c (coll? chromosome1))
            (c (coll? chromosome2))]
      :post [(c (coll? %))]}
-    (simulated-binary 0.5 1 chromosome1 chromosome2))
+    (simulated-binary 0.9 0.5 1 chromosome1 chromosome2))
   
-  ([p nu chromosome1 chromosome2]
-    {:pre [(not-negnum? p)
+  ([probability gene-probability nu chromosome1 chromosome2]
+    {:pre [(not-negnum? probability)
+           (not-negnum? gene-probability)
            (not-negnum? nu)
            (c (coll? chromosome1))
            (c (coll? chromosome2))]
      :post [(c (coll? %))]}
-    (let [new-genes (map (fn [gene1 gene2]
-                           (if (< (rand) p)
-                             (simulated-binary-cross gene1 gene2 nu)
-                             [gene1 gene2]))
-                         chromosome1 chromosome2)]
-      [(map first new-genes) (map second new-genes)])))
+    (if (< (rand) probability) 
+      (let [new-genes (map (fn [gene1 gene2]
+                             (if (< (rand) gene-probability)
+                               (simulated-binary-cross gene1 gene2 nu)
+                               [gene1 gene2]))
+                           chromosome1 chromosome2)]
+        [(map first new-genes) (map second new-genes)])
+      [chromosome1 chromosome2])))
+
+
+
+
 
