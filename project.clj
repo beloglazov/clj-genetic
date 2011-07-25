da50a25c59bf72b0c33647867e9d5b109c3e5aec
Anton Beloglazov <anton.beloglazov@gmail.com>
2011-07-25 16:41:49 +1000

    Updated the readme

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clj-genetic "1.0.0-SNAPSHOT"
-  :description "A library for solving optimization problems using Genetic Algorithms"
+(defproject clj-genetic "0.1.0-SNAPSHOT"
+  :description "A Clojure library for solving optimization problems using real-coded Genetic Algorithms"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[midje "1.2-alpha4"]])
