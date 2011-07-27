aecdf6a23d87c6eba0bd389581c1ba2cb83f00bb
Anton Beloglazov <anton.beloglazov@gmail.com>
2011-07-27 12:23:51 +1000

    Made delta-max a function argument for parameter-based-mutate; added function descriptions

--- src/clj_genetic/mutation.clj
+++ src/clj_genetic/mutation.clj
@@ -1,7 +1,12 @@
 (ns clj-genetic.mutation
   (:use clj-genetic.util))
 
-(defn parameter-based-mutate? [t-max t n]
+(defn parameter-based-mutate?
+  "Probabilistic decision making of whether to mutate a gene
+   t-max - maximum number of generations allowed
+   t - current generation
+   n - number of genes in a chromosome"
+  [t-max t n]
   {:pre [(c (not-negnum? t-max))
          (c (not-negnum? t))
          (c (posnum? n))]
@@ -12,56 +17,43 @@
     (< (rand) p)))
 
 (defn parameter-based-mutate
-  
-  ([nu-base t gene]
+  "Perform a mutation for the parameter-based mutation operator
+   nu-base - the base for calculating nu = nu-base + t
+   delta-max - maximum allowed perturbance of a gene
+   t - current generation
+   gene - gene to mutate"
+  ([nu-base delta-max t gene]
     {:pre [(c (not-negnum? nu-base))
+           (c (posnum? delta-max))
            (c (not-negnum? t))
            (c (number? gene))]
      :post [(c (number? %))]}
     (let [nu (+ nu-base t)
           u (rand)
-          delta-max (* gene 100) ; can be adjusted
           delta (if (<= u 0.5)
                   (- (Math/pow (* 2 u) (/ 1 (+ nu 1))) 1)
                   (- 1 (Math/pow (* 2 (- 1 u)) (/ 1 (+ nu 1)))))]
-      (+ gene (* delta delta-max))))
-  
-  ([limits nu-base t gene]
-    {:pre [(c (contains-keys? limits :min :max))           
-           (c (not-negnum? nu-base))
-           (c (not-negnum? t))
-           (c (number? gene))]
-     :post [(c (number? %))]}
-    (let [nu (+ nu-base t)
-          u (rand)
-          delta-max (- (:max limits) (:min limits))
-          d (/ (min (- gene (:min limits)) (- (:max limits) gene)) delta-max)
-          delta (if (<= u 0.5)
-                  (- (Math/pow (+ (* 2 u)
-                                  (* (- 1 (* 2 u))
-                                     (Math/pow (- 1 d)
-                                               (+ nu 1))))
-                               (/ 1 (+ nu 1))) 
-                     1)
-                  (- 1 (Math/pow (+ (* 2 (- 1 u))
-                                    (* 2
-                                       (- u 0.5)
-                                       (Math/pow (- 1 d)
-                                                 (+ nu 1)))) 
-                                 (/ 1 (+ nu 1)))))]
       (+ gene (* delta delta-max)))))
 
-(defn parameter-based 
-  
-  ([t-max t genes]
-    {:pre [(c (not-negnum? t-max))
+(defn parameter-based
+  "Parameter-based mutation operator
+   delta-max - maximum allowed perturbance of a gene
+   t-max - maximum number of generations allowed
+   t - current generation
+   genes - a set of genes to mutate
+   nu-base - the base for calculating nu = nu-base + t
+   default nu-base = 100"  
+  ([delta-max t-max t genes]
+    {:pre [(c (posnum? delta-max))
+           (c (not-negnum? t-max))
            (c (not-negnum? t))
            (c (coll? genes))]
      :post [(c (coll? %))]}
-    (parameter-based 100 t-max t genes))
+    (parameter-based 100 delta-max t-max t genes))
   
-  ([nu t-max t genes]
-    {:pre [(c (posnum? nu))
+  ([nu-base delta-max t-max t genes]
+    {:pre [(c (posnum? nu-base))
+           (c (posnum? delta-max))
            (c (not-negnum? t-max))
            (c (not-negnum? t))
            (c (coll? genes))]
@@ -69,12 +61,48 @@
     (let [n (count genes)]
       (map (fn [gene]
              (if (parameter-based-mutate? t-max t n)
-               (parameter-based-mutate nu t gene)
+               (parameter-based-mutate nu-base delta-max t gene)
                gene))
            genes))))
 
+(defn parameter-based-with-limits-mutate 
+  "limits - limits of possible gene values
+   nu-base - the base for calculating nu = nu-base + t
+   t - current generation
+   gene - gene to mutate" 
+  [limits nu-base t gene]
+  {:pre [(c (contains-keys? limits :min :max))           
+         (c (not-negnum? nu-base))
+         (c (not-negnum? t))
+         (c (number? gene))]
+   :post [(c (number? %))]}
+  (let [nu (+ nu-base t)
+        u (rand)
+        delta-max (- (:max limits) (:min limits))
+        d (/ (min (- gene (:min limits)) (- (:max limits) gene)) delta-max)
+        delta (if (<= u 0.5)
+                (- (Math/pow (+ (* 2 u)
+                                (* (- 1 (* 2 u))
+                                   (Math/pow (- 1 d)
+                                             (+ nu 1))))
+                             (/ 1 (+ nu 1))) 
+                   1)
+                (- 1 (Math/pow (+ (* 2 (- 1 u))
+                                  (* 2
+                                     (- u 0.5)
+                                     (Math/pow (- 1 d)
+                                               (+ nu 1)))) 
+                               (/ 1 (+ nu 1)))))]
+    (+ gene (* delta delta-max))))
+
 (defn parameter-based-with-limits
-    
+  "Parameter-based mutation operator supporting genes limits
+   limits - limits of possible gene values
+   t-max - maximum number of generations allowed
+   t - current generation
+   genes - a set of genes to mutate
+   nu-base - the base for calculating nu = nu-base + t
+   default nu-base = 100"     
   ([limits t-max t genes]
     {:pre [(c (coll? limits))
            (c (not-negnum? t-max))
@@ -83,9 +111,9 @@
      :post [(c (coll? %))]}
     (parameter-based-with-limits limits 100 t-max t genes))
   
-  ([limits nu t-max t genes]
+  ([limits nu-base t-max t genes]
     {:pre [(c (coll? limits))
-           (c (posnum? nu))
+           (c (posnum? nu-base))
            (c (not-negnum? t-max))
            (c (not-negnum? t))
            (c (coll? genes))]
@@ -93,7 +121,7 @@
     (let [n (count genes)]
       (map (fn [gene-limits gene]
              (if (parameter-based-mutate? t-max t n)
-               (parameter-based-mutate gene-limits nu t gene)
+               (parameter-based-with-limits-mutate gene-limits nu-base t gene)
                gene))
            limits genes))))
 
--- test/clj_genetic/test/mutation.clj
+++ test/clj_genetic/test/mutation.clj
@@ -11,6 +11,7 @@
 (def t-max 500)
 (def n 3)
 (def nu 100)
+(def delta-max 1)
 
 (fact 
   "1/3+(5/500)(1 − 1/3) = 0.34"
@@ -28,15 +29,6 @@
   (provided (rand) => 1.0))
 
 (fact
-  (parameter-based-with-limits limits nu t-max t genes) => genes
-  (provided (parameter-based-mutate? t-max t n) => false)
-  (parameter-based-with-limits limits nu t-max t genes) => (just 5 5 5)
-  (provided (parameter-based-mutate? t-max t n) => true
-            (parameter-based-mutate anything
-                                    nu t 
-                                    anything) => 5))
-
-(fact
   "Parameter-based mutation with parameter limits
    d = min(x - 0, 30 - x) / (30 - 0) = 1/30
    d-max = 30 - 0 = 30
@@ -48,25 +40,41 @@
    = 1-(2*(1-0.8)+2*(0.8-0.5)*(1-1/30)^(100+5+1))^(1/(100+5+1)) =  
    = 0.008228868 
    y = x + delta * d-max = 1+0.008228868*30 = 1.24686604"  
-  (parameter-based-mutate {:min 0 :max 30} t nu 1) => (roughly 0.8608)
+  (parameter-based-with-limits-mutate {:min 0 :max 30} t nu 1) => (roughly 0.8608)
   (provided (rand) => 0.3)
-  (parameter-based-mutate {:min 0 :max 30} t nu 1) => (roughly 1.2468)
+  (parameter-based-with-limits-mutate {:min 0 :max 30} t nu 1) => (roughly 1.2468)
   (provided (rand) => 0.8))
 
 (fact
   "Parameter-based mutation without parameter limits
-   d-max = 1 * 100 = 100
+   d-max = 1
    delta = (2*u)^(1/(nu+t+1))-1 = (2*0.3)^(1/(100+5+1))-1 = 
    = −0.004807516
-   y = x + delta * d-max = 1−0.004807516*100 = 0.5192484
-   delta = 1-(2*(1-u))^(1/(nu+t+1)) = 1-(2*(1-0.8))^(1/(100+5+1)) = 
-   = 1-(2*(1-0.8)+2*(0.8-0.5)*(1-1/30)^(100+5+1))^(1/(100+5+1)) =  
+   y = x + delta * d-max = 1−0.004807516*1 = 0.995192484
+   delta = 1-(2*(1-u))^(1/(nu+t+1)) = 1-(2*(1-0.8))^(1/(100+5+1)) =  
    = 0.008606998
-   y = x + delta * d-max = 1+0.008606998*100 = 1.8606998"  
-  (parameter-based-mutate t nu 1) => (roughly 0.5192)
+   y = x + delta * d-max = 1+0.008606998*1 = 1.008606998"  
+  (parameter-based-mutate nu delta-max t 1) => (roughly 0.9951)
   (provided (rand) => 0.3)
-  (parameter-based-mutate t nu 1) => (roughly 1.8606)
+  (parameter-based-mutate nu delta-max t 1) => (roughly 1.0086)
   (provided (rand) => 0.8))
+
+
+(fact
+  (parameter-based nu delta-max t-max t genes) => genes
+  (provided (parameter-based-mutate? t-max t n) => false)
+  (parameter-based nu delta-max t-max t genes) => (just 5 5 5)
+  (provided (parameter-based-mutate? t-max t n) => true
+            (parameter-based-mutate nu delta-max t anything) => 5))
+
+(fact
+  (parameter-based-with-limits limits nu t-max t genes) => genes
+  (provided (parameter-based-mutate? t-max t n) => false)
+  (parameter-based-with-limits limits nu t-max t genes) => (just 5 5 5)
+  (provided (parameter-based-mutate? t-max t n) => true
+            (parameter-based-with-limits-mutate anything
+                                                nu t 
+                                                anything) => 5))
 
 
 
