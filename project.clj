919bb9eafbd2e21f22a37f9479c9293b0bd2ad44
Anton Beloglazov <anton.beloglazov@gmail.com>
2011-07-27 17:31:54 +1000

    Updated midje to 1.2-beta1

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "A Clojure library for solving optimization problems using real-coded Genetic Algorithms"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [midje "1.2-alpha4"]])
+                 [midje "1.2-beta1"]])
