0e03db7db77ad9c8eeee4d5a36066b4a7d07233e
Anton Beloglazov <anton.beloglazov@gmail.com>
2011-07-27 12:49:06 +1000

    Added function descriptions

--- src/clj_genetic/random_generators.clj
+++ src/clj_genetic/random_generators.clj
@@ -2,22 +2,28 @@
   (:use clj-genetic.util))
 
 (defn rand-from [from to]
+  "Generates a random values with from-to limits
+   from - minimum allowed value
+   to - maximum allowed value"
   {:pre [(c (number? from))
          (c (number? to))]
    :post [(c (number? %))]}
   (+ from (rand (- to from))))
 
 (defn generate-population
-  "If the limits are not specified, the values are generated in [-1000, 1000]"
+  "Generates a random population of single-variable chromosomes. 
+   If the limits are not specified, the values are generated in [-1000, 1000].
+   size - size of the population
+   limits - limits on gene values"
   
-  ([n]
-    {:pre [(c (posnum? n))]
+  ([size]
+    {:pre [(c (posnum? size))]
      :post [(c (coll? %))]}
-    (generate-population n [{:min -1000 
+    (generate-population size [{:min -1000 
                              :max 1000}]))
   
-  ([n limits]
-    {:pre [(c (posnum? n))
+  ([size limits]
+    {:pre [(c (posnum? size))
            (c (coll? limits))]
      :post [(c (coll? %))]}
     (map 
@@ -25,15 +31,30 @@
         (map 
           (fn [{from :min to :max}] (rand-from from to))
           limits)) 
-      (range n))))
+      (range size))))
 
-(defn generate-population-n-vars [n vars]
-  {:pre [(c (posnum? n))
-         (c (posnum? vars))]
-   :post [(c (coll? %))]}
-  (apply map vector
-         (take vars (repeatedly 
-                      #(map first (generate-population n))))))
+(defn generate-population-n-vars 
+  "Generates a random population of n-variable chromosomes.
+   If the limits are not specified, the values are generated in [-1000, 1000].
+   size - size of the population
+   vars - number of variables in a chromosome
+   limits - limits on gene values"
+  ([size vars]  
+    {:pre [(c (posnum? size))
+           (c (posnum? vars))]
+     :post [(c (coll? %))]}
+    (apply map vector
+           (take vars (repeatedly 
+                        #(map first (generate-population size))))))
+  
+  ([size vars limits]  
+    {:pre [(c (posnum? size))
+           (c (posnum? vars))
+           (c (coll? limits))]
+     :post [(c (coll? %))]}
+    (apply map vector
+           (take vars (repeatedly 
+                        #(map first (generate-population size limits)))))))
 
 
 
