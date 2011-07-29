e7ccac09ff525ca6115c630035de64e7920dcef7
Anton Beloglazov <anton.beloglazov@gmail.com>
2011-07-29 19:32:31 +1000

    Updated the version of clj-predicates

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "A Clojure library for solving optimization problems using real-coded Genetic Algorithms"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [clj-predicates "0.1.0"]
+                 [clj-predicates "0.1.1"]
                  [midje "1.2-beta1"]])
