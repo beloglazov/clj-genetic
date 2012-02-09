2c59e41ac89f9fab77ec2409ebe484d6501656be
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-02-10 10:40:07 +1100

    Version -> 0.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-genetic "0.3.0-SNAPSHOT"
+(defproject clj-genetic "0.3.1-SNAPSHOT"
   :description "A Clojure library for solving optimization problems using real-coded Genetic Algorithms"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-predicates "0.2.0"]
